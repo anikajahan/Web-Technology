8194be4e9ede07ab627d78011662045e1b6e4189
anikajahan <106438573+anikajahan@users.noreply.github.com>
2022-08-22 23:39:19 +0600

    Add files via upload

---
+++ Lab Task 8/Security/view/Admin_home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Home</title>
+
+</head>
+<body style="background-color:#ccffff;">
+
+<fieldset>
+
+House Management Company
+<div align=right>
+<?php
+include 'C:/xampp/htdocs/project/Lab Task 8/Admin/Navigation/header.php';?>
+</div>
+ </fieldset>
+ 
+
+
+<fieldset>
+<div align=center><img src='/project/Final Project/logo.jpg' alt="HM Management" width="150" height="150"></div>
+<h2 style="color: Green; font-size: 30px; text-align:center"; >You are in Admin Section</h2>
+ <br><br><br><br>
+ </fieldset>
+ 
+ <fieldset>
+ <div align=center>
+<?php include 'C:/xampp/htdocs/project/Lab Task 8/Admin/Navigation/footer.php';?>
+</div>
+</fieldset>
+</body>
+</html>
